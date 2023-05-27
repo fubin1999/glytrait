a18e2eccb1c1aa897c32bf1585e6e4cadb18ceec
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2023-05-27 09:52:23 +0800

    Add trait.py.
    Add `build_meta_property_table` function to trait.py.

---
+++ glytrait/trait.py
@@ -0,0 +1,57 @@
+from collections.abc import Iterable
+
+import pandas as pd
+
+from .glycan import NGlycan
+
+
+def build_meta_property_table(
+    glycan_ids: Iterable[str], glycans: Iterable[NGlycan]
+) -> pd.DataFrame:
+    """Build a table of meta properties for glycans.
+
+    The following meta properties are included:
+        - isComplex: Whether the glycan is a complex type.
+        - isHighMannose: Whether the glycan is a high-mannose type.
+        - isHybrid: Whether the glycan is a hybrid type.
+        - isBisecting: Whether the glycan has a bisection.
+        - is1Antennary: Whether the glycan has a 1-antenna.
+        - is2Antennary: Whether the glycan has a 2-antenna.
+        - is3Antennary: Whether the glycan has a 3-antenna.
+        - is4Antennary: Whether the glycan has a 4-antenna.
+        - totalAntenna: The total number of antennae.
+        - coreFuc: The number of fucoses on the core.
+        - antennaryFuc: The number of fucoses on the antenna.
+        - totalFuc: The total number of fucoses.
+        - totalSia: The total number of sialic acids.
+        - totalMan: The total number of mannoses.
+        - totalGal: The total number of galactoses.
+
+    Args:
+        glycan_ids (Iterable[str]): The IDs of the glycans. Could be the
+            accession, or the composition.
+        glycans (Iterable[NGlycan]): The glycans.
+
+    Returns:
+        pd.DataFrame: The table of meta properties, with glycan IDs as the index,
+            and the property names as the columns.
+    """
+    meta_property_table = pd.DataFrame(index=list(glycan_ids))
+
+    meta_property_table["isComplex"] = [g.is_complex() for g in glycans]
+    meta_property_table["isHighMannose"] = [g.is_high_mannose() for g in glycans]
+    meta_property_table["isHybrid"] = [g.is_hybrid() for g in glycans]
+    meta_property_table["isBisecting"] = [g.is_bisecting() for g in glycans]
+    meta_property_table["is1Antennary"] = [g.count_antenna() == 1 for g in glycans]
+    meta_property_table["is2Antennary"] = [g.count_antenna() == 2 for g in glycans]
+    meta_property_table["is3Antennary"] = [g.count_antenna() == 3 for g in glycans]
+    meta_property_table["is4Antennary"] = [g.count_antenna() == 4 for g in glycans]
+    meta_property_table["totalAntenna"] = [g.count_antenna() for g in glycans]
+    meta_property_table["coreFuc"] = [g.count_core_fuc() for g in glycans]
+    meta_property_table["antennaryFuc"] = [g.count_antennary_fuc() for g in glycans]
+    meta_property_table["totalFuc"] = [g.count_fuc() for g in glycans]
+    meta_property_table["totalSia"] = [g.count_sia() for g in glycans]
+    meta_property_table["totalMan"] = [g.count_man() for g in glycans]
+    meta_property_table["totalGal"] = [g.count_gal() for g in glycans]
+
+    return meta_property_table
