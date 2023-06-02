f0d38bd33355064a0dc1ae83d84cdb9ea7313dbd
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2023-06-02 17:56:40 +0800

    Move `run_workflow` to core.py.
    In order for gui.py to use this function.

--- glytrait/cli.py
+++ glytrait/cli.py
@@ -1,17 +1,12 @@
 from pathlib import Path
-from typing import Optional
 
 import click
 import emoji
 
+from glytrait.core import run_workflow
 from glytrait.exception import GlyTraitError
-from glytrait.io import read_input, write_output
 from glytrait.trait import (
-    save_trait_formula_template,
-    load_formulas,
-    build_meta_property_table,
-    calcu_derived_trait,
-    calcu_direct_trait
+    save_trait_formula_template
 )
 
 UNDIFINED = "__UNDEFINED__"
@@ -102,23 +97,6 @@ def cli(input_file, output_file, sia_linkage, formula_file):
         raise click.UsageError(str(e) + emoji.emojize(" :thumbs_down:"))
     msg = f"Done :thumbs_up:! Output written to {output_file}."
     click.echo(emoji.emojize(msg))
-
-
-def run_workflow(
-    input_file: str,
-    output_file: str,
-    sia_linkage: bool = False,
-    user_formula_file: Optional[str] = None,
-) -> None:
-    """Run the workflow."""
-    glycans, abund_df = read_input(input_file)
-    formulas = load_formulas(user_formula_file)
-    if not sia_linkage:
-        formulas = [f for f in formulas if f.sia_linkage is False]
-    meta_prop_df = build_meta_property_table(abund_df.columns, glycans, sia_linkage)
-    derived_traits = calcu_derived_trait(abund_df, meta_prop_df, formulas)
-    direct_traits = calcu_direct_trait(abund_df)
-    write_output(output_file, derived_traits, direct_traits, meta_prop_df, formulas)
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,58 +1,9 @@
 from pathlib import Path
-from unittest.mock import Mock, MagicMock
 
 import pytest
 from click.testing import CliRunner
 
 from glytrait import cli
-
-
-@pytest.mark.parametrize("sia_linkage", [True, False])
-def test_run_workflow(mocker, sia_linkage):
-    abund_df_mock = Mock()
-    abund_df_mock.columns = ["col1", "col2"]
-    formulas_mock = [MagicMock(), MagicMock(), MagicMock()]
-    formulas_mock[0].sia_linkage = False
-    formulas_mock[1].sia_linkage = True
-    formulas_mock[2].sia_linkage = False
-
-    read_input_mock = mocker.patch(
-        "glytrait.cli.read_input", return_value=("glycans", abund_df_mock)
-    )
-    load_formulas_mock = mocker.patch(
-        "glytrait.cli.load_formulas", return_value=formulas_mock
-    )
-    build_meta_property_table_mock = mocker.patch(
-        "glytrait.cli.build_meta_property_table", return_value="meta_prop_df"
-    )
-    calcu_derived_traits_mock = mocker.patch(
-        "glytrait.cli.calcu_derived_trait", return_value="derived_trait_df"
-    )
-    calcu_direct_traits_mock = mocker.patch(
-        "glytrait.cli.calcu_direct_trait", return_value="direct_trait_df"
-    )
-    write_output_mock = mocker.patch("glytrait.cli.write_output")
-
-    cli.run_workflow("input_file", "output_file", sia_linkage, "user_formula_file")
-
-    read_input_mock.assert_called_once_with("input_file")
-    load_formulas_mock.assert_called_once_with("user_formula_file")
-    build_meta_property_table_mock.assert_called_once_with(
-        ["col1", "col2"], "glycans", sia_linkage
-    )
-
-    if sia_linkage is False:
-        formulas_mock.pop(1)
-    calcu_derived_traits_mock.assert_called_once_with(
-        abund_df_mock, "meta_prop_df", formulas_mock
-    )
-    write_output_mock.assert_called_once_with(
-        "output_file",
-        "derived_trait_df",
-        "direct_trait_df",
-        "meta_prop_df",
-        formulas_mock,
-    )
 
 
 def test_cli_save_template(clean_dir):
