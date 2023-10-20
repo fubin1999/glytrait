33c5ee415d54ce078191aee593b4013ba320c153
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2023-10-20 11:27:52 +0800

    Update the logic of child-parent relationship.
    - "A1S", "A2S", etc. are not children of "CS" anymore.
    - "xxFa" and "xxFc" are not children of "xxF" anymore.
    - "xxS" are not children of "xxGS" anymore.
    - "xxE" and "xxL" are not children of "xxS" anymore.
    
    Previous logic is too radical, linking traits with different meanings. For example, the "Fa" series reflects the activity of FUT8, thus is inproper to be regarded as the "F" series, which reflects fucosylation level generally.

--- glytrait/formula.py
+++ glytrait/formula.py
@@ -186,13 +186,6 @@ class TraitFormula:
         Note that A2FSG is not a child of A2G, for it is a grandchild of A2G.
         Also, A2FG is not a child of CG.
         """
-        if self.name in ("A1S", "A2S", "A3S", "A4S") and other.name == "CS":
-            return True
-        if self.name in ("A1E", "A2E", "A3E", "A4E") and other.name == "CE":
-            return True
-        if self.name in ("A1L", "A2L", "A3L", "A4L") and other.name == "CL":
-            return True
-
         num1 = set(self.numerator_properties)
         den1 = set(self.denominator_properties)
         try:
@@ -201,38 +194,13 @@ class TraitFormula:
         except AttributeError:
             raise TypeError("The other formula is not a TraitFormula instance.")
 
-        # General case
-        condition_1 = (
+        condition = (
             not (num2 - num1)
             and not (den2 - den1)
             and num1 - num2 == den1 - den2
             and len(num1 - num2) == 1
         )
-
-        # Fc, Fa are children of F. E, L are children of S.
-        special_mp = {
-            "totalFuc": ["coreFuc", "antennaryFuc"],
-            "totalSia": ["a23Sia", "a26Sia"],
-        }
-        condition_2 = (
-            den1 == den2
-            and len(num2 - num1) == 1
-            and list(num2 - num1)[0] in special_mp
-            and len(num1 - num2) == 1
-            and list(num1 - num2)[0] in special_mp[list(num2 - num1)[0]]
-        )
-
-        # GS is a child of S.
-        condition_3 = (
-            num1 == num2
-            and ("totalSia" in num1 or "a23Sia" in num1 or "a26Sia" in num1)
-            and len(den1 - den2) == 1
-            and list(den1 - den2)[0] == "totalGal"
-        )
-
-        if condition_1 or condition_2 or condition_3:
-            return True
-        return False
+        return condition
 
 
 def load_formulas(
