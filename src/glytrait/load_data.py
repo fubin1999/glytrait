24c410bf2701eed6ed2e4e6168b3fd2561078774
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2023-12-27 23:41:11 +0800

    feature: add `load_structures`

--- src/glytrait/load_data.py
+++ src/glytrait/load_data.py
@@ -1,13 +1,16 @@
-from collections.abc import Iterable
+import csv
+from collections.abc import Iterable, Sequence
 from pathlib import Path
 from typing import NewType
 
 import pandas as pd
 
 from glytrait.exception import FileTypeError, FileFormatError
+from glytrait.glycan import parse_structures, StructureDict
 
 AbundanceTable = NewType("AbundanceTable", pd.DataFrame)
 """Abundance table type.
+The index are glycans and the columns are samples.
 The abundance table could only be returned by `load_abundance_table` function.
 """
 
@@ -39,6 +42,35 @@ def load_abundance_table(filepath: str) -> AbundanceTable:
     return AbundanceTable(df)
 
 
+def load_structures(filepath: str) -> StructureDict:
+    """Load structures from filepath.
+
+    Args:
+        filepath: Path to structures file.
+
+    Returns:
+        StructureDict: A dict with glycan ids with keys and
+            `Structure` objects as values.
+
+    Raises:
+        FileTypeError: If the file is not a csv file.
+        FileNotFoundError: If the file does not exist.
+        FileFormatError: If the file format is incorrect.
+            This includes: (1) the file has duplicated glycan ids; (2) the file has
+            duplicated structures; (3) the file does not have a "GlycanID" column
+            or a "Structure" column; (4) the file has extra columns.
+        StructureParseError: If the structure cannot be parsed.
+            All glycan ids not parsed will be listed in the error message.
+    """
+    _check_file_type(filepath, "csv")
+    _check_exist(filepath)
+    _check_columns("glycan structures", filepath, ["GlycanID", "Structure"], only=True)
+    ids, strings = _load_glycans(filepath, "Structure")
+    _check_duplicated_items("glycan ids", ids)
+    _check_duplicated_items("glycan structures", strings)
+    return parse_structures(zip(ids, strings))
+
+
 def _check_file_type(filepath: str, file_type: str) -> None:
     if Path(filepath).suffix != f".{file_type}":
         raise FileTypeError(f"File {filepath} is not a {file_type} file.")
@@ -65,6 +97,11 @@ def _check_duplicated_columns(df_name: str, df: pd.DataFrame) -> None:
             raise FileFormatError(f"The {df_name} should not have duplicated columns.")
 
 
+def _check_duplicated_items(item_name: str, to_check: Sequence[str]) -> None:
+    if len(set(to_check)) != len(to_check):
+        raise FileFormatError(f"The {item_name} should not be duplicated.")
+
+
 def _check_columns(
     filename: str, filepath: str, columns: Iterable[str], only: bool = False
 ) -> None:
@@ -82,3 +119,15 @@ def _check_columns(
         if not set(columns) <= set(header):
             msg = f"The {filename} should have columns {_format_columns()}."
             raise FileFormatError(msg)
+
+
+def _load_glycans(filepath: str, string_col: str) -> tuple[list[str], list[str]]:
+    """Helper function for `load_structures` and `load_compositions`."""
+    ids: list[str] = []
+    strings: list[str] = []
+    with open(filepath) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            ids.append(row["GlycanID"])
+            strings.append(row[string_col])
+    return ids, strings
--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import pytest
 
-from glytrait.load_data import load_abundance_table
-from glytrait.exception import FileTypeError, FileFormatError
+from glytrait.load_data import load_abundance_table, load_structures
+from glytrait.exception import FileTypeError, FileFormatError, StructureParseError
 
 
 @pytest.fixture
@@ -68,3 +68,73 @@ class TestLoadAbundanceTable:
         abundance_table.to_csv(filepath, index=True)
         with pytest.raises(FileFormatError):
             load_abundance_table(filepath)
+
+
+@pytest.fixture
+def structure_file(clean_dir) -> str:
+    filepath = str(clean_dir / "structure.csv")
+    df = pd.DataFrame(
+        {
+            "GlycanID": ["G1", "G2", "G3"],
+            "Structure": ["glycoct1", "glycoct2", "glycoct3"],
+        }
+    )
+    df.to_csv(filepath, index=False)
+    return filepath
+
+
+@pytest.fixture
+def patch_structure_from_string(monkeypatch):
+    def mock_return(name, string):
+        if string == "invalid":
+            raise StructureParseError
+        return string
+
+    monkeypatch.setattr("glytrait.glycan.Structure.from_string", mock_return)
+
+
+@pytest.mark.usefixtures("patch_structure_from_string")
+class TestLoadStructures:
+    def test_basic(self, structure_file):
+        result = load_structures(structure_file)
+        assert result == {
+            "G1": "glycoct1",
+            "G2": "glycoct2",
+            "G3": "glycoct3",
+        }
+
+    def test_not_csv(self, clean_dir):
+        filepath = clean_dir / "structure.txt"
+        filepath.touch()
+        with pytest.raises(FileTypeError):
+            load_structures(filepath)
+
+    def test_not_exist(self, clean_dir):
+        filepath = clean_dir / "structure.csv"
+        with pytest.raises(FileNotFoundError):
+            load_structures(filepath)
+
+    @pytest.mark.parametrize("column", ["GlycanID", "Structure"])
+    def test_missing_columns(self, structure_file, clean_dir, column):
+        df = pd.read_csv(structure_file)
+        df = df.drop(column, axis=1)
+        filepath = str(clean_dir / "structure.csv")
+        df.to_csv(filepath, index=False)
+        with pytest.raises(FileFormatError):
+            load_structures(filepath)
+
+    def test_duplicated_glycan_ids(self, structure_file, clean_dir):
+        df = pd.read_csv(structure_file)
+        df.loc[2, "GlycanID"] = "G1"
+        filepath = str(clean_dir / "structure.csv")
+        df.to_csv(filepath, index=False)
+        with pytest.raises(FileFormatError):
+            load_structures(filepath)
+
+    def test_duplicated_structure_strings(self, structure_file, clean_dir):
+        df = pd.read_csv(structure_file)
+        df.loc[2, "Structure"] = "glycoct1"
+        filepath = str(clean_dir / "structure.csv")
+        df.to_csv(filepath, index=False)
+        with pytest.raises(FileFormatError):
+            load_structures(filepath)
