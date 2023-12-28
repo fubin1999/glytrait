826a8e0c12bda2a1c4ea6b9043de4981f92b1fe8
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2023-12-28 22:18:17 +0800

    feature: add `export_all` to data_export.py and add support to exporting DataFrames

---
+++ src/glytrait/data_export.py
@@ -0,0 +1,68 @@
+from collections.abc import Iterable, Callable
+from pathlib import Path
+from typing import Type, TypeVar, Any
+
+import pandas as pd
+
+
+def export_all(to_export: Iterable[tuple[str, Any]], base_path: str) -> None:
+    """Export all data to the directory given by `base_path`.
+
+    Args:
+        to_export: An iterable of tuples (filename, data object).
+        base_path: The folder to export data into.
+    """
+    for filename, data in to_export:
+        exporter = _fetch_exporter(data)
+        filepath = str(Path(base_path) / filename)
+        exporter(data, filepath)
+
+
+ExportRegistryKey = tuple[Type, bool]
+"""key: the object type, value: whether it is a list of objects"""
+
+Exporter = Callable[[Any, str], None]
+"""A function that exports an object to a file.
+The first argument is the object to export, the second is the filepath.
+"""
+
+export_registry: dict[ExportRegistryKey, Exporter] = {}
+
+
+def register_exporter(data_type: Type, is_list: bool = False):  # type: ignore
+    """Decorator for regitering an exporter for a certain type."""
+    T = TypeVar("T", bound=Exporter)
+
+    def decorator(exporter_class: T) -> T:
+        export_registry[(data_type, is_list)] = exporter_class
+        return exporter_class
+
+    return decorator
+
+
+def _fetch_exporter(single_data: Any) -> Exporter:
+    """Factory function for creating an exporter for a data object."""
+    is_list = isinstance(single_data, list)
+    if is_list:
+        try:
+            data_type = type(single_data[0])
+        except IndexError:
+            return dummy_exporter
+    else:
+        data_type = type(single_data)
+    exporter = export_registry.get((data_type, is_list))
+    if exporter is not None:
+        return exporter
+    else:
+        raise ValueError(f"Unsupported data type for export: {data_type}.")
+
+
+def dummy_exporter(data: Any, filepath: str) -> None:
+    """Dummy exporter that does nothing."""
+    pass
+
+
+@register_exporter(pd.DataFrame)
+def export_dataframe(df: pd.DataFrame, filepath: str) -> None:
+    """Export a pandas DataFrame to a CSV file."""
+    df.to_csv(filepath, index=True)
---
+++ tests/test_data_export.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pytest
+
+from glytrait.data_export import export_all
+
+
+class TestExportAll:
+    def test_export_dataframe(self, clean_dir):
+        df = pd.DataFrame(
+            {
+                "a": [1, 2],
+                "b": [3, 4],
+            },
+            index=pd.Index([1, 2], name="index"),
+        )
+        filename = "test.csv"
+
+        export_all([(filename, df)], clean_dir)
+
+        assert (clean_dir / filename).exists()
+        assert (clean_dir / filename).read_text() == ("index,a,b\n" "1,1,3\n" "2,2,4\n")
