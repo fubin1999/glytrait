105f04de34ae780bcf1fd72408b8a8d3dee0c95b
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-02-14 15:25:36 +0800

    feature: vectorize calculation of meta-properties

--- tests/unit/test_meta_property.py
+++ tests/unit/test_meta_property.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import pytest
+from attrs import define
 
 from glytrait import meta_property as mp
+from glytrait.meta_property import Glycan
 from .glycoct import *
 
 
@@ -12,9 +14,27 @@ def test_build_meta_property_table(mocker):
         return_value=["mp1", "mp2"],
     )
 
+    @define
+    class MP1(mp.MetaProperty):
+        name = "mp1"
+        supported_mode = "both"
+        return_type = "UInt8"
+
+        def calculate_one(self, glycan: Glycan) -> int:
+            return 1
+
+    @define
+    class MP2(mp.MetaProperty):
+        name = "mp2"
+        supported_mode = "both"
+        return_type = "UInt8"
+
+        def calculate_one(self, glycan: Glycan) -> int:
+            return int(glycan[-1])
+
     mps = {
-        "mp1": lambda glycoct: 1,
-        "mp2": lambda glycoct: int(glycoct[-1]),
+        "mp1": MP1(),
+        "mp2": MP2(),
     }
     mocker.patch(
         "glytrait.meta_property.get_meta_property",
@@ -24,15 +44,22 @@ def test_build_meta_property_table(mocker):
 
     glycans = {"G1": "glycan1", "G2": "glycan2", "G3": "glycan3"}
     result = mp.build_meta_property_table(glycans, "structure", True)
-    expected = pd.DataFrame({"mp1": [1, 1, 1], "mp2": [1, 2, 3]}, index=glycans)
+    expected = pd.DataFrame(
+        {"mp1": [1, 1, 1], "mp2": [1, 2, 3]}, index=glycans, dtype="UInt8"
+    )
     pd.testing.assert_frame_equal(result, expected)
 
 
 @pytest.fixture
 def register_some_mp():
-    @mp.mp("some_mp", "both")
-    def some_mp(glycan):
-        return 0
+    @mp.register
+    class SomeMetaProperty(mp.MetaProperty):
+        name = "some_mp"
+        supported_mode = "both"
+        return_type = "UInt8"
+
+        def calculate_one(self, glycan: Glycan) -> int:
+            return 0
 
     yield
     mp._mp_objects.pop("some_mp")
@@ -45,9 +72,9 @@ def test_register(register_some_mp):
 def test_register_duplicate(register_some_mp):
     with pytest.raises(ValueError):
 
-        @mp.mp("some_mp", "both")
-        def some_mp(glycan):
-            return 0
+        @mp.register
+        class OtherMetaProperty(mp.MetaProperty):
+            name = "some_mp"
 
 
 @pytest.mark.parametrize(
@@ -109,20 +136,27 @@ def test_get_mp_object(register_some_mp):
     assert mp.get_meta_property("some_mp").name == "some_mp"
 
 
+def test_calculate_many(register_some_mp):
+    glycans = {"G1": "glycan1", "G2": "glycan2"}
+    result = mp.get_meta_property("some_mp").calculate_many(glycans)
+    expected = pd.Series([0, 0], index=glycans, dtype="UInt8", name="some_mp")
+    pd.testing.assert_series_equal(result, expected)
+
+
 # !!! All tests on meta-properties should use this function for conciseness. !!!
 def _test_meta_property(meta_property, string, expected, make_glycan):
     """Test a meta-property on a glycan string.
 
     Args:
-        meta_property (mp.MetaProperty): Meta property function to test.
-        string (str): Glycan string. Either GlycoCT or composition.
-        expected (Any): Expected result.
-        make_glycan (Callable[[str], glyc.Structure]): Factory fixture to make a
+        meta_property: Meta property function to test.
+        string: Glycan string. Either GlycoCT or composition.
+        expected: Expected result.
+        make_glycan: Factory fixture to make a
             glycan from a string. Either `make_structure` or `make_composition`.
             See `conftest.py` for these two fixtures.
     """
     glycan = make_glycan(string)
-    assert meta_property(glycan) == expected
+    assert meta_property().calculate_one(glycan) == expected
 
 
 @pytest.mark.parametrize(
@@ -138,7 +172,7 @@ def _test_meta_property(meta_property, string, expected, make_glycan):
     ],
 )
 def test_count_antenna_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_antenna_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountAntennaMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -150,7 +184,7 @@ def test_count_antenna_mp(glycoct, expected, make_structure):
     ],
 )
 def test_count_fuc_mp_struc(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_fuc_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountFucMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -162,7 +196,7 @@ def test_count_fuc_mp_struc(glycoct, expected, make_structure):
     ],
 )
 def test_count_fuc_mp_comp(comp, expected, make_composition):
-    _test_meta_property(mp.count_fuc_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountFucMP, comp, expected, make_composition)
 
 
 @pytest.mark.parametrize(
@@ -174,7 +208,7 @@ def test_count_fuc_mp_comp(comp, expected, make_composition):
     ],
 )
 def test_count_core_fuc_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_core_fuc_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountCoreFucMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -186,7 +220,7 @@ def test_count_core_fuc_mp(glycoct, expected, make_structure):
     ],
 )
 def test_count_antennary_fuc_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_antennary_fuc_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountAntennaryFucMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -200,7 +234,7 @@ def test_count_antennary_fuc_mp(glycoct, expected, make_structure):
     ],
 )
 def test_count_sia_struc_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_sia_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountSiaMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -212,7 +246,7 @@ def test_count_sia_struc_mp(glycoct, expected, make_structure):
     ],
 )
 def test_count_sia_comp_mp(comp, expected, make_composition):
-    _test_meta_property(mp.count_sia_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountSiaMP, comp, expected, make_composition)
 
 
 @pytest.mark.parametrize(
@@ -228,7 +262,7 @@ def test_count_sia_comp_mp(comp, expected, make_composition):
     ],
 )
 def test_glycan_type_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.glycan_type_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.GlycanTypeMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -242,7 +276,7 @@ def test_glycan_type_mp(glycoct, expected, make_structure):
     ],
 )
 def test_bisection_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.bisection_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.BisectionMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -254,7 +288,7 @@ def test_bisection_mp(glycoct, expected, make_structure):
     ],
 )
 def test_count_man_mp_struc(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_man_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountManMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -266,7 +300,7 @@ def test_count_man_mp_struc(glycoct, expected, make_structure):
     ],
 )
 def test_count_man_mp_comp(comp, expected, make_composition):
-    _test_meta_property(mp.count_man_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountManMP, comp, expected, make_composition)
 
 
 @pytest.mark.parametrize(
@@ -278,7 +312,7 @@ def test_count_man_mp_comp(comp, expected, make_composition):
     ],
 )
 def test_count_gal_mp_struc(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_gal_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountGalMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -290,7 +324,7 @@ def test_count_gal_mp_struc(glycoct, expected, make_structure):
     ],
 )
 def test_count_gal_mp_comp(comp, expected, make_composition):
-    _test_meta_property(mp.count_gal_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountGalMP, comp, expected, make_composition)
 
 
 @pytest.mark.parametrize(
@@ -302,7 +336,7 @@ def test_count_gal_mp_comp(comp, expected, make_composition):
     ],
 )
 def test_count_glcnac_mp_struc(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_glcnac_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountGlcNAcMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -314,7 +348,7 @@ def test_count_glcnac_mp_struc(glycoct, expected, make_structure):
     ],
 )
 def test_count_glcnac_mp_comp(comp, expected, make_composition):
-    _test_meta_property(mp.count_glcnac_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountGlcNAcMP, comp, expected, make_composition)
 
 
 @pytest.mark.parametrize(
@@ -325,7 +359,7 @@ def test_count_glcnac_mp_comp(comp, expected, make_composition):
     ],
 )
 def test_has_poly_lacnac_mp(glycoct, expected, make_structure):
-    _test_meta_property(mp.has_poly_lacnac_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.HasPolyLacNAcMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -337,7 +371,7 @@ def test_has_poly_lacnac_mp(glycoct, expected, make_structure):
     ],
 )
 def test_count_a23_sia_mp_struc(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_a23_sia_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountA23SiaMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -350,7 +384,7 @@ def test_count_a23_sia_mp_struc(glycoct, expected, make_structure):
     ],
 )
 def test_count_a23_sia_mp_comp(comp, expected, make_composition):
-    _test_meta_property(mp.count_a23_sia_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountA23SiaMP, comp, expected, make_composition)
 
 
 @pytest.mark.parametrize(
@@ -362,7 +396,7 @@ def test_count_a23_sia_mp_comp(comp, expected, make_composition):
     ],
 )
 def test_count_a26_sia_mp_struc(glycoct, expected, make_structure):
-    _test_meta_property(mp.count_a26_sia_mp, glycoct, expected, make_structure)
+    _test_meta_property(mp.CountA26SiaMP, glycoct, expected, make_structure)
 
 
 @pytest.mark.parametrize(
@@ -375,4 +409,4 @@ def test_count_a26_sia_mp_struc(glycoct, expected, make_structure):
     ],
 )
 def test_count_a26_sia_mp_comp(comp, expected, make_composition):
-    _test_meta_property(mp.count_a26_sia_mp, comp, expected, make_composition)
+    _test_meta_property(mp.CountA26SiaMP, comp, expected, make_composition)
