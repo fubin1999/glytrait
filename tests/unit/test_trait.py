391830a4f331f33e2d70c883018cac0a7600624f
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-03-17 16:36:47 +0800

    test: update tests for adopting the new `TraitFormula`

--- tests/unit/test_trait.py
+++ tests/unit/test_trait.py
@@ -1,37 +1,30 @@
 from itertools import permutations
 
 import pandas as pd
-import pytest
+from attrs import define
 
-from glytrait.formula import TraitFormula
 from glytrait.trait import calcu_derived_trait
 
-pytestmark = pytest.mark.skip("`TraitFormula` to be updated.")
+
+@define
+class FakeFormula:
+
+    name: str
+    value: float
+
+    def initialize(self, *args, **kwargs):
+        return
+
+    def calcu_trait(self, abundance_df: pd.DataFrame):
+        return pd.Series(
+            [self.value] * len(abundance_df.index),
+            index=abundance_df.index,
+            name=self.name
+        )
 
 
-@pytest.fixture
-def trait_formulas() -> list[TraitFormula]:
-    return [
-        TraitFormula(
-            description="Relative abundance of high mannose type glycans within total spectrum",
-            name="TM",
-            type="structure",
-            numerator_properties=["isHighMannose"],
-            denominator_properties=["."],
-        ),
-        TraitFormula(
-            description="The ratio of high-mannose to hybrid glycans",
-            name="MHy",
-            type="structure",
-            numerator_properties=["isHighMannose"],
-            denominator_properties=["isHybrid"],
-        ),
-    ]
-
-
-@pytest.fixture
-def abundance_table() -> pd.DataFrame:
-    return pd.DataFrame(
+def test_calcu_derived_trait(mocker):
+    abund_df = pd.DataFrame(
         {
             "G1": [1, 2, 3],
             "G2": [4, 5, 6],
@@ -41,43 +34,14 @@ def abundance_table() -> pd.DataFrame:
         dtype=float,
     )
 
+    formulas = [FakeFormula('all1', 1), FakeFormula('all2', 2)]
 
-@pytest.fixture
-def meta_property_table() -> pd.DataFrame:
-    return pd.DataFrame(
+    result = calcu_derived_trait(abund_df, mocker.Mock(), formulas)
+    expected = pd.DataFrame(
         {
-            ".": [1, 1, 1],
-            "isHighMannose": [True, False, False],
-            "isHybrid": [False, False, True],
-            "isComplex": [False, True, False],
+            "all1": [1, 1, 1],
+            "all2": [2, 2, 2],
         },
-        index=["G1", "G2", "G3"],
+        index=["S1", "S2", "S3"]
     )
-
-
-@pytest.fixture
-def expected_derived_trait_table() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "TM": [1 / 12, 2 / 15, 3 / 18],
-            "MHy": [1 / 7, 2 / 8, 3 / 9],
-        },
-        index=["S1", "S2", "S3"],
-    )
-
-
-def test_calcu_trait(
-    trait_formulas, abundance_table, meta_property_table, expected_derived_trait_table
-):
-    result = calcu_derived_trait(abundance_table, meta_property_table, trait_formulas)
-    pd.testing.assert_frame_equal(result, expected_derived_trait_table)
-
-
-def test_calcu_trait_glycan_order_not_the_same(
-    trait_formulas, abundance_table, meta_property_table, expected_derived_trait_table
-):
-    glycans = abundance_table.columns.tolist()
-    for perm in permutations(glycans):
-        new_mp_table = meta_property_table.reindex(perm)
-        result = calcu_derived_trait(abundance_table, new_mp_table, trait_formulas)
-        pd.testing.assert_frame_equal(result, expected_derived_trait_table)
+    pd.testing.assert_frame_equal(result, expected)
