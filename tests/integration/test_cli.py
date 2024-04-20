2086ba2a0fadc683f82a29c1433845fcf1109478
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-04-20 12:05:14 +0800

    test: add integration test to CLI

---
+++ tests/integration/test_cli.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner
+
+from glytrait.cli import cli
+
+
+def copy_data_file(from_: str | Path, to_: str | Path):
+    """Copy data file to a new location."""
+    with open(from_, "r") as f:
+        data = f.read()
+    with open(to_, "w") as f:
+        f.write(data)
+
+
+def save_abundance_table(path: str | Path):
+    """Save abundance table to a file."""
+    data_file = "tests/integration/data/abundance.csv"
+    copy_data_file(data_file, path)
+
+
+def save_structures(path: str | Path):
+    """Save structures to a file."""
+    data_file = "tests/integration/data/structures.csv"
+    copy_data_file(data_file, path)
+
+
+def save_groups(path: str | Path):
+    """Save groups to a file."""
+    data_file = "tests/integration/data/groups.csv"
+    copy_data_file(data_file, path)
+
+
+class TestCLI:
+
+    def test_basic(self, tmp_path):
+        runner = CliRunner()
+        abund_path = str(tmp_path / "abundance.csv")
+        struct_path = str(tmp_path / "structures.csv")
+        save_abundance_table(abund_path)
+        save_structures(struct_path)
+        result = runner.invoke(cli, [abund_path, struct_path])
+        assert result.exit_code == 0
+        assert (tmp_path / "abundance_glytrait/derived_traits.csv").exists()
+        assert (tmp_path / "abundance_glytrait/derived_traits_filtered.csv").exists()
+        assert (tmp_path / "abundance_glytrait/glycan_abundance_processed.csv").exists()
+        assert (tmp_path / "abundance_glytrait/meta_properties.csv").exists()
