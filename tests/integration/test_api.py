31aa5da9b1d0bf4186c9951f6d8bfb6e6ea999d2
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-04-24 00:08:47 +0800

    feature: add `Experiment`
    This is the main api of glytrait. The old api classes `GlyTrait` will be removed later.

---
+++ tests/integration/test_api.py
@@ -0,0 +1,32 @@
+import pytest
+import pandas as pd
+
+from glytrait.load_data import load_data
+from glytrait.api import Experiment
+
+
+class TestExperiment:
+
+    @pytest.fixture
+    def input_data(self):
+        abund_df = pd.read_csv("tests/integration/data/abundance.csv")
+        glycan_df = pd.read_csv("tests/integration/data/structures.csv")
+        group_df = pd.read_csv("tests/integration/data/groups.csv")
+        return load_data(abund_df, glycan_df, group_df, mode="structure")
+
+    @pytest.fixture
+    def experiment(self, input_data):
+        return Experiment(input_data)
+
+    def test_whole_workflow(self, experiment):
+        experiment.preprocess()
+        experiment.extract_meta_properties()
+        experiment.derive_traits()
+        experiment.post_filter()
+        experiment.diff_analysis()
+
+        assert isinstance(experiment.processed_abundance_table, pd.DataFrame)
+        assert isinstance(experiment.meta_property_table, pd.DataFrame)
+        assert isinstance(experiment.derived_trait_table, pd.DataFrame)
+        assert isinstance(experiment.filtered_derived_trait_table, pd.DataFrame)
+        assert isinstance(experiment.diff_results, dict)
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,8 +1,11 @@
-from typing import Any
+from collections import namedtuple
+from typing import Any, ClassVar
 
+import pandas as pd
 import pytest
 from attrs import define, field
 
+import glytrait.api
 from glytrait.api import _Config, GlyTrait
 from glytrait import api as glytrait_api
 
@@ -323,3 +326,295 @@ class TestGlyTrait:
         glytrait_api.calcu_derived_trait.assert_called_once_with(
             "abundance_table", "meta_property_table", "formulas"
         )
+
+
+@define
+class SimpleWorkflow(glytrait_api._Workflow):
+    _all_steps: ClassVar = ["step1", "step2", "step3"]
+    _data_dict: ClassVar = {
+        "step1": ["data1"],
+        "step2": ["data2"],
+        "step3": ["data3_1", "data3_2"],
+    }
+
+    @glytrait_api._step
+    def step1(self, return_value=None):
+        if return_value:
+            return {"data1": return_value}
+        return {"data1": "abcde"}
+
+    @glytrait_api._step
+    def step2(self):
+        return {"data2": 12345}
+
+    @glytrait_api._step
+    def step3(self):
+        return {"data3_1": "xyz", "data3_2": 67890}
+
+
+class TestWorkflow:
+
+    def test_call_step1(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        assert workflow.get_data("data1") == "abcde"
+
+    def test_call_step1_and_step2(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        workflow.step2()
+        assert workflow.get_data("data1") == "abcde"
+        assert workflow.get_data("data2") == 12345
+
+    def test_call_step1_step2_and_step3(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        workflow.step2()
+        workflow.step3()
+        assert workflow.get_data("data1") == "abcde"
+        assert workflow.get_data("data2") == 12345
+        assert workflow.get_data("data3_1") == "xyz"
+        assert workflow.get_data("data3_2") == 67890
+
+    def test_recall_step1_no_force(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        with pytest.raises(glytrait_api.InvalidOperationOrderError):
+            workflow.step1()
+
+    def test_recall_step1_with_force(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        workflow.step1(return_value="new_value", force=True)
+        assert workflow.get_data("data1") == "new_value"
+
+    def test_reset_data(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        workflow.step2()
+        workflow.step1(force=True)
+        assert workflow.get_data("data1") == "abcde"
+        with pytest.raises(glytrait_api.MissingDataError):
+            workflow.get_data("data2")
+
+    def test_call_step2_without_step1(self):
+        workflow = SimpleWorkflow()
+        with pytest.raises(glytrait_api.InvalidOperationOrderError):
+            workflow.step2()
+
+    def test_call_step3_without_step2(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        with pytest.raises(glytrait_api.InvalidOperationOrderError):
+            workflow.step3()
+
+    def test_call_step3_without_step1(self):
+        workflow = SimpleWorkflow()
+        with pytest.raises(glytrait_api.InvalidOperationOrderError):
+            workflow.step3()
+
+    def test_current_step(self):
+        workflow = SimpleWorkflow()
+        assert workflow.current_step == "__START__"
+
+        workflow.step1()
+        assert workflow.current_step == "step1"
+
+        workflow.step2()
+        assert workflow.current_step == "step2"
+
+        workflow.step3()
+        assert workflow.current_step == "step3"
+
+        workflow.step1(force=True)
+        assert workflow.current_step == "step1"
+
+    def test_reset(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        workflow.step2()
+        workflow.reset()
+        assert workflow.current_step == "__START__"
+        with pytest.raises(glytrait_api.MissingDataError):
+            workflow.get_data("data1")
+        with pytest.raises(glytrait_api.MissingDataError):
+            workflow.get_data("data2")
+
+    def test_get_data_not_generated_yet(self):
+        workflow = SimpleWorkflow()
+        with pytest.raises(glytrait_api.MissingDataError):
+            workflow.get_data("data1")
+
+    def test_get_data_not_exist(self):
+        workflow = SimpleWorkflow()
+        workflow.step1()
+        with pytest.raises(KeyError):
+            workflow.get_data("not_exist")
+
+
+@define
+class FakeInputData:
+    abundance_table: pd.DataFrame
+    glycans: dict[str, str]
+    groups: pd.Series
+
+
+class TestExperiment:
+
+    @pytest.fixture
+    def abundance_table(self):
+        return pd.DataFrame(
+            {
+                "G1": [1.0, 2.0, 3.0],
+                "G2": [4.0, 5.0, 6.0],
+                "G3": [7.0, 8.0, 9.0],
+            },
+            index=pd.Index(["S1", "S2", "S3"], name="Sample"),
+        )
+
+    @pytest.fixture
+    def glycans(self):
+        return {"G1": "Glycan1", "G2": "Glycan2", "G3": "Glycan3"}
+
+    @pytest.fixture
+    def groups(self):
+        return pd.Series(
+            data=["A", "B", "A"],
+            index=pd.Index(["S1", "S2", "S3"], name="Sample"),
+            name="Group",
+        )
+
+    @pytest.fixture
+    def input_data(self, abundance_table, glycans, groups):
+        return FakeInputData(
+            abundance_table=abundance_table, glycans=glycans, groups=groups
+        )
+
+    @pytest.fixture
+    def exp(self, mocker, input_data):
+        mocker.patch("glytrait.api.Experiment.reset")
+        return glytrait_api.Experiment(input_data)
+
+    def test_abundance_table_getter(self, exp, abundance_table):
+        assert exp.abundance_table.equals(abundance_table)
+
+    def test_abundance_table_setter(self, exp):
+        exp.abundance_table = pd.DataFrame()
+        assert exp.abundance_table.empty
+        exp.reset.assert_called_once()
+
+    def test_glycans_getter(self, exp, glycans):
+        assert exp.glycans == glycans
+
+    def test_glycans_setter(self, exp):
+        exp.glycans = {}
+        assert exp.glycans == {}
+        exp.reset.assert_called_once()
+
+    def test_groups_getter(self, exp, groups):
+        assert exp.groups.equals(groups)
+
+    def test_groups_setter(self, exp):
+        exp.groups = pd.Series()
+        assert exp.groups.empty
+        exp.reset.assert_called_once()
+
+    @pytest.mark.parametrize("filter", [0.0, 1.0])
+    @pytest.mark.parametrize("impute_method", ["zero", "min"])
+    def test_preprocess(
+        self, mocker, input_data, abundance_table, filter, impute_method
+    ):
+        mocker.patch("glytrait.api.preprocess", return_value="result")
+        exp = glytrait_api.Experiment(input_data)
+        exp.preprocess(filter, impute_method)
+        assert exp.processed_abundance_table == "result"
+        called_args = glytrait_api.preprocess.call_args.kwargs
+        assert called_args["data"].equals(abundance_table)
+        assert called_args["filter_max_na"] == filter
+        assert called_args["impute_method"] == impute_method
+
+    def test_extract_meta_properties(self, mocker, input_data, abundance_table):
+        mocker.patch("glytrait.api.build_meta_property_table", return_value="result")
+        input_data.glycans["G4"] = "Glycan4"
+        exp = glytrait_api.Experiment(input_data)
+        exp._data["processed_abundance_table"] = abundance_table
+        exp._current_step = "preprocess"
+        exp.extract_meta_properties()
+        assert exp.meta_property_table == "result"
+        glytrait_api.build_meta_property_table.assert_called_once_with(
+            {"G1": "Glycan1", "G2": "Glycan2", "G3": "Glycan3"}, "structure", False
+        )
+
+    @pytest.fixture
+    def patch_for_derive_traits(self, mocker):
+        mocker.patch("glytrait.api.load_default_formulas", return_value="formulas")
+        mocker.patch("glytrait.api.parse_formulas")
+        mocker.patch("glytrait.api.calcu_derived_trait", return_value="trait_table")
+
+    @pytest.fixture
+    def exp_for_derive_traits(self, exp):
+        exp._data["processed_abundance_table"] = "abund_table"
+        exp._data["meta_property_table"] = "mp_table"
+        exp._current_step = "extract_meta_properties"
+        return exp
+
+    @pytest.mark.usefixtures("patch_for_derive_traits")
+    def test_derive_traits_with_default_formulas(self, exp_for_derive_traits):
+        exp_for_derive_traits.derive_traits()
+        assert exp_for_derive_traits.derived_trait_table == "trait_table"
+        assert exp_for_derive_traits.get_data("formulas") == "formulas"
+        glytrait.api.parse_formulas.assert_not_called()
+        glytrait.api.calcu_derived_trait.assert_called_once()
+
+    @pytest.mark.usefixtures("patch_for_derive_traits")
+    def test_derive_traits_with_provided_formulas(self, mocker, exp_for_derive_traits):
+        FakeFormula = namedtuple("FakeFormula", "name sia_linkage")
+        fake_formulas = [FakeFormula("F1", False), FakeFormula("F2", False)]
+        mocker.patch("glytrait.api.parse_formulas", return_value=fake_formulas)
+        exp_for_derive_traits.derive_traits(["expr1", "expr2"])
+        assert exp_for_derive_traits.derived_trait_table == "trait_table"
+        assert exp_for_derive_traits.get_data("formulas") == fake_formulas
+        glytrait.api.load_default_formulas.assert_not_called()
+        glytrait.api.calcu_derived_trait.assert_called_once()
+
+    @pytest.mark.usefixtures("patch_for_derive_traits")
+    def test_derive_traits_with_provided_sia_formulas_no_sia_mode(
+        self, mocker, exp_for_derive_traits
+    ):
+        FakeFormula = namedtuple("FakeFormula", "name sia_linkage")
+        fake_formulas = [FakeFormula("F1", True), FakeFormula("F2", False)]
+        mocker.patch("glytrait.api.parse_formulas", return_value=fake_formulas)
+        with pytest.raises(ValueError):
+            exp_for_derive_traits.derive_traits(["expr1", "expr2"])
+
+    @pytest.mark.parametrize("corr_threshold", [1.0, 0.5])
+    def test_post_filter(self, mocker, exp, corr_threshold):
+        mocker.patch("glytrait.api.post_filter", return_value="filtered")
+        exp._data["formulas"] = "formulas"
+        exp._data["derived_trait_table"] = "trait_table"
+        exp._current_step = "derive_traits"
+        exp.post_filter(corr_threshold)
+        glytrait.api.post_filter.assert_called_once_with(
+            formulas="formulas",
+            trait_df="trait_table",
+            threshold=corr_threshold,
+            method="pearson",
+        )
+        assert exp.filtered_derived_trait_table == "filtered"
+
+    @pytest.fixture
+    def patch_for_diff_analysis(self, mocker):
+        mocker.patch("glytrait.api.t_test", return_value="t_test_result")
+        mocker.patch("glytrait.api.anova", return_value="anova_result")
+
+    @pytest.fixture
+    def exp_for_diff_analysis(self, exp):
+        exp._data["filtered_derived_trait_table"] = "trait_table"
+        exp._current_step = "post_filter"
+        return exp
+
+    @pytest.mark.usefixtures("patch_for_diff_analysis")
+    def test_diff_analysis(self, exp_for_diff_analysis):
+        exp_for_diff_analysis.input_data.groups = None
+        with pytest.raises(glytrait.api.MissingDataError):
+            exp_for_diff_analysis.diff_analysis()
