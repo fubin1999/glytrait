6745a35614a687a1850b635d64e0e5181f33ca39
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-04-28 15:03:18 +0800

    feature: add a spinner when streamlit app is running

--- src/glytrait_streamlit/app.py
+++ src/glytrait_streamlit/app.py
@@ -218,13 +218,14 @@ def prepare_zip(exp):
 
 st.markdown("---")
 if st.button("Run GlyTrait"):
-    exp = Experiment(input_data, mode=mode, sia_linkage=sia_linkage)
-    with capture_glytrait_error():
-        exp.run_workflow(
-            filter_max_na=glycan_filter_threshold,
-            impute_method=impute_method,
-            corr_threshold=post_filter_threshold,
-        )
+    with st.spinner("Running..."):
+        exp = Experiment(input_data, mode=mode, sia_linkage=sia_linkage)
+        with capture_glytrait_error():
+            exp.run_workflow(
+                filter_max_na=glycan_filter_threshold,
+                impute_method=impute_method,
+                corr_threshold=post_filter_threshold,
+            )
     st.success("Succeed! Click the button below to download the results.")
     with tempfile.TemporaryDirectory() as tmp_dir:
         zip_bytes = prepare_zip(exp)
